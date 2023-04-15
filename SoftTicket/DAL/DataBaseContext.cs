877a24d66337151416395766ade04f6871f63784
JULIAN ANDRES GÓMEZ MORENO <juliangomez250755@correo.itm.edu.co>
2023-04-15 15:28:43 -0500

    DAL - The Class DBContext has been created. Controller - The Controller Ticket has been created. View - The CRUD for Ticket has been created. Entities - The Class Entitiy has been created. - The Class Ticket has been created. Program - The references to DB Context has been created. AppSetting - The connection to DB has been created.

---
+++ SoftTicket/DAL/DataBaseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftTicket.DAL.Entities;
+
+namespace SoftTicket.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+        }
+
+        // Creación Tablas
+        public DbSet<Ticket> Tickets { get; set; }
+
+        // Creación Indice Tablas
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Ticket>().HasIndex(t => t.Id).IsUnique();
+        }
+
+    }
+}
---
+++ SoftTicket/DAL/Entities/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace SoftTicket.DAL.Entities
+{
+    public class Entity
+    {
+        [Key]
+        public virtual Guid Id { get; set; }
+
+        [Display(Name = "Fecha Creación")]
+        public virtual DateTime? CreateDate { get; set; }
+
+        [Display(Name = "Fecha Modificación")]
+        public virtual DateTime? ModifiedDate { get; set; }
+    }
+}
+
