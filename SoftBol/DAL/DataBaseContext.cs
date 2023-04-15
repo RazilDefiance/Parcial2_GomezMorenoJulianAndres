98388a6f191e0a1eda6c072756b1a5ce273813bc
JULIAN ANDRES GÓMEZ MORENO <juliangomez250755@correo.itm.edu.co>
2023-04-15 14:45:36 -0500

    DAL - ContextDB has been created. Entities - Entity has been created. - Ticket has been created. Controller - TicketsController has been created. Views - The CRUD for Tickets has been created.

---
+++ SoftBol/DAL/DataBaseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftBol.DAL.Entities;
+
+namespace SoftBol.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+        }
+
+        // Creación Tablas
+        public DbSet<Ticket> Tickets { get; set; }
+
+        // Creación Indice Tablas
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Ticket>().HasIndex(t => t.Id).IsUnique();
+        }
+
+    }
+}
---
+++ SoftBol/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using SoftBol.DAL;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+// MyCode Jagomo -------------------------------------------------------------------------
+builder.Services.AddDbContext<DataBaseContext>(
+    o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+    );
+
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+// ---------------------------------------------------------------------------------------
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
